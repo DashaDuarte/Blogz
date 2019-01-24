ecb3422cfbf0c1b3679354c652be0cf1ff136c21
Dasha Duarte <duartedangel@gmail.com>
2019-01-23 21:04:37 -0500

    Final4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import request, redirect, render_template
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
     
-app = flask(__name__)
+app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:pass123@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
